94fd906f6c0f769e430df76ffa8feeb543c846c1
Christophe Badoit <c.badoit@lesiteimmo.com>
2020-05-07 14:57:05 +0200

    fixes: updates headers

--- belotecoinche.game.php
+++ belotecoinche.game.php
@@ -2,7 +2,7 @@
 /**
  *------
  * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
- * BeloteCoinche implementation : © <Your name here> <Your email address here>
+ * BeloteCoinche implementation : © Christophe Badoit <gameboardarena@tof2k.com>
  *
  * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
  * See http://en.boardgamearena.com/#!doc/Studio for more information.
